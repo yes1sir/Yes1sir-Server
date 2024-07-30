8caedd0fbb6dfe982cf6e0ca4f503bb4e4b3c5ac
shinminkyoung1 <dottiee99@naver.com>
2024-07-30 22:38:12 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/yes1sir/yessir/YessirApplication.java
@@ -0,0 +1,13 @@
+package yes1sir.yessir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YessirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YessirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yes1sir/yessir/YessirApplicationTests.java
@@ -0,0 +1,13 @@
+package yes1sir.yessir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YessirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
