33485f7320780e691aa0334aeffb3cf4de66e7bb
shinminkyoung1 <dottiee99@naver.com>
2024-08-05 04:36:57 +0900

    feat: 폴더 추가

--- src/main/java/yes1sir/yessir/repository/ProductReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package yes1sir.yessir.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import yes1sir.yessir.model.ProductReview;
-
-public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
-}
