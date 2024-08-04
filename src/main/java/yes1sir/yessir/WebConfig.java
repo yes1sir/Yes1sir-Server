ae680fe98b3105380904bd171878ea112896621e
shinminkyoung1 <dottiee99@naver.com>
2024-08-05 06:24:34 +0900

    feat: 리다이렉트 설정 변경

--- src/main/java/yes1sir/yessir/WebConfig.java
+++ src/main/java/yes1sir/yessir/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://yourdomain.com")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
